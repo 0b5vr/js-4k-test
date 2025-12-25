0cac9c749b46b4c246856408535c8f4e29b67aab
0b5vr <0b5vr@0b5vr.com>
2025-12-26 01:45:15 +0900

    size: html/ui part, optimize size
    -6 bytes

--- src/ui.ts
+++ src/ui.ts
@@ -2,12 +2,4 @@ import uiHtml from './assets/ui.html?html';
 
 document.body.innerHTML = uiHtml;
 
-/**
- * The button to start the experience.
- */
-export const button = document.querySelector('button') as HTMLButtonElement;
-
-/**
- * The main canvas element.
- */
-export const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+export const [button, canvas] = document.body.childNodes as unknown as [HTMLParagraphElement, HTMLCanvasElement];
